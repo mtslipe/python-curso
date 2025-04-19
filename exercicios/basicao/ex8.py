f5de291d6a57f85511a56f1030ea8512267abc88
Felipe Maximiano <160420882+mtslipe@users.noreply.github.com>
2025-04-19 02:47:41 -0300

    Update ex8.py

--- exercicios/basicao/ex8.py
+++ exercicios/basicao/ex8.py
@@ -1,3 +1,11 @@
 # Exercício Python #008 - Conversor de Medidas
 
-num = float(input(""))
+num = float(input("Uma distancia em metros: "))
+
+print(f"""A medida de {num}m corresponde a 
+{num/1000}km
+{num/100}hm
+{num/10}dam
+{num*10}dm
+{num*100}cm
+{num*1000}mm""")

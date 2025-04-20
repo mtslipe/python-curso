da458d7da5a8d6b8bf2b04a04ccfab34306e7a13
Felipe Maximiano <160420882+mtslipe@users.noreply.github.com>
2025-04-20 19:16:11 -0300

    Update ex12.py

--- exercicios/basicao/11-20/ex12.py
+++ exercicios/basicao/11-20/ex12.py
@@ -1,12 +1,13 @@
 # Exercício Python #012 - Calculando Descontos
 
-def aplicarDesconto():
-    valor = preco - ((porcentagemDesconto / 100) * preco) # porcentagem de desconto sobre cem, vezes o preco = valor de desconto. preco - valor de desconto.
-    return valor
-
-
+class Desconto:
+    def aplicarDesconto(preco : float, porcentagemDesconto : int):
+        valor = preco - ((porcentagemDesconto / 100) * preco) # porcentagem de desconto sobre cem, vezes o preco = valor de desconto. preco - valor de desconto.
+        return valor
+    
+desconto = Desconto()
 
 preco = float(input("Qual o preço do produto? R$"))
 porcentagemDesconto = int(input("Qual o desconto? %")) # por cento
 
-print(f"\nO produto que custava R${preco:.2f}, na promoção com desconto de {porcentagemDesconto}% vai custar R${aplicarDesconto():.2f}")
+print(f"\nO produto que custava R${preco:.2f}, na promoção com desconto de {porcentagemDesconto}% vai custar R${desconto.aplicarDesconto(preco, porcentagemDesconto):.2f}")
